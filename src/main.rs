b4ca059a84a259c3c06ac4a583a0086293ad8c2e
Federico Amedeo Izzo <federico@izzo.pro>
2024-04-05 19:59:30 +0200

    Refactor main code to Camera struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bluest::{pairing::NoInputOutputPairingAgent, Adapter, Uuid, Characteristic};
-use futures_lite::StreamExt;
+use futures_lite::{StreamExt, Stream};
 use instax_pal::*;
-use std::error::Error;
+use std::{error::Error, pin::Pin};
 use num_traits::FromPrimitive;
 use std::{thread, time::Duration};
 use chrono::prelude::*;
@@ -113,104 +113,118 @@ impl Packet {
     }
 }
 
+struct Camera {
+    write_char: Characteristic,
+    notify_stream: Pin<Box<dyn Stream<Item = Result<Vec<u8>, bluest::Error>> + Send + Unpin>>,
+}
+impl Camera {
+    async fn new() -> Result<Self, Box<dyn Error>> {
+        let adapter = Adapter::default()
+            .await
+            .ok_or("Bluetooth adapter not found")?;
+        adapter.wait_available().await?;
+        println!("Searching Instax device");
+        let device = adapter
+            .discover_devices(&[INSTAX_SERVICE_UUID])
+            .await?
+            .next()
+            .await
+            .ok_or("Failed to discover device")??;
+        println!("found device: {} ({:?})",
+            device.name().as_deref().unwrap_or("(unknown)"),
+            device.id()
+        );
+        // HACK: on some laptops we need to repeat pairing, otherwise we can't connect
+        if device.is_paired().await? {
+            println!("Repeating device pairing");
+            device.unpair().await?
+        }
+        device.pair_with_agent(&NoInputOutputPairingAgent).await?;
+        adapter.connect_device(&device).await?;
+        println!("connected!");
+        let service = match device
+            .discover_services_with_uuid(INSTAX_SERVICE_UUID)
+            .await?
+            .get(0)
+        {
+            Some(service) => service.clone(),
+            None => return Err("Service not found".into()),
+        };
+        let characteristics = service.characteristics().await?;
+        let write_char = characteristics
+            .iter()
+            .find(|x| x.uuid() == INSTAX_WRITE_UUID)
+            .ok_or("write characteristic not found")?
+            .clone();
+        let notify_char = characteristics
+            .iter()
+            .find(|x| x.uuid()  == INSTAX_NOTIFY_UUID)
+            .ok_or("notify characteristic not found")?
+            .clone();
+        let notify_stream: Pin<Box<dyn Stream<Item = Result<Vec<u8>, bluest::Error>> + Send + Unpin>> = Box::pin(notify_char.notify().await?);
+        let camera = Camera{write_char, notify_stream};
+        Ok(camera)
+    }
+
+    async fn send_packet(&mut self, packet: Packet) -> Result<(), Box<dyn Error>> {
+        let data = packet.pack();
+        println!("SENT: {:x?}", &data);
+        self.write_char.write(&data).await?;
+        Ok(())
+    }
+    
+    async fn receive_packet(&mut self) -> Option<Packet> {
+        while let Some(msg) = self.notify_stream.next().await {
+            let data = &msg.unwrap();
+            println!("RECV: {:x?}", &data);
+            let packet = Packet::unpack(&data);
+            return Some(packet);
+        }
+        None
+    }
+    
+    async fn receive_data(&mut self) -> Option<Vec<u8>> {
+        let mut data: Vec<u8> = Vec::new();
+        while let Some(msg) = self.notify_stream.next().await {
+            let payload = msg.unwrap();
+            data.extend(&payload);
+            println!("RECV: {:x?}", &payload);
+            if payload.len() == 1 {
+                return Some(data)
+            }
+        }
+        None
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let adapter = Adapter::default()
-        .await
-        .ok_or("Bluetooth adapter not found")?;
-    adapter.wait_available().await?;
-    println!("Searching Instax device");
-    let device = adapter
-        .discover_devices(&[INSTAX_SERVICE_UUID])
-        .await?
-        .next()
-        .await
-        .ok_or("Failed to discover device")??;
-    println!("found device: {} ({:?})",
-        device.name().as_deref().unwrap_or("(unknown)"),
-        device.id()
-    );
-    // HACK: on some laptops we need to repeat pairing, otherwise we can't connect
-    if device.is_paired().await? {
-        println!("Repeating device pairing");
-        device.unpair().await?
-    }
-    device.pair_with_agent(&NoInputOutputPairingAgent).await?;
-    adapter.connect_device(&device).await?;
-    println!("connected!");
-    let service = match device
-        .discover_services_with_uuid(INSTAX_SERVICE_UUID)
-        .await?
-        .get(0)
-    {
-        Some(service) => service.clone(),
-        None => return Err("Service not found".into()),
-    };
-    let characteristics = service.characteristics().await?;
-    let write_char = characteristics
-        .iter()
-        .find(|x| x.uuid() == INSTAX_WRITE_UUID)
-        .ok_or("write characteristic not found")?;
-    let notify_char = characteristics
-        .iter()
-        .find(|x| x.uuid() == INSTAX_NOTIFY_UUID)
-        .ok_or("notify characteristic not found")?;
-    support_function_version_info(write_char, notify_char).await;
-    parameter_read(write_char, notify_char, ReadWriteSettingType::TRANSFER_FORMAT).await;
-    parameter_read(write_char, notify_char, ReadWriteSettingType::FLASH_SETTING).await;
-    set_timedate(write_char, notify_char).await;
-    support_function_info(write_char, notify_char, SupportFunctionInfoType::IMAGE_SUPPORT_INFO).await;
-    support_function_info(write_char, notify_char, SupportFunctionInfoType::BATTERY_INFO).await;
-    support_function_info(write_char, notify_char, SupportFunctionInfoType::CAMERA_FUNCTION_INFO).await;
-    support_function_info(write_char, notify_char, SupportFunctionInfoType::CAMERA_HISTORY_INFO).await;
-    // automatic_photo_download(write_char, notify_char).await;
-    live_view_test(write_char, notify_char).await;
+    let mut camera = Camera::new().await?;
+    support_function_version_info(&mut camera).await;
+    parameter_read(&mut camera, ReadWriteSettingType::TRANSFER_FORMAT).await;
+    parameter_read(&mut camera, ReadWriteSettingType::FLASH_SETTING).await;
+    set_timedate(&mut camera).await;
+    support_function_info(&mut camera, SupportFunctionInfoType::IMAGE_SUPPORT_INFO).await;
+    support_function_info(&mut camera, SupportFunctionInfoType::BATTERY_INFO).await;
+    support_function_info(&mut camera, SupportFunctionInfoType::CAMERA_FUNCTION_INFO).await;
+    support_function_info(&mut camera, SupportFunctionInfoType::CAMERA_HISTORY_INFO).await;
+    //automatic_photo_download(&mut camera).await;
+    live_view_test(&mut camera).await;
     Ok(())
 }
 
-async fn send_packet(write_char: &Characteristic, packet: Packet) {
-    let data = packet.pack();
-    println!("SENT: {:x?}", &data);
-    write_char.write(&data).await.unwrap();
-}
-
-async fn receive_packet(notify_char: &Characteristic) -> Option<Packet> {
-    let mut updates = notify_char.notify().await.unwrap();
-    while let Some(msg) = updates.next().await {
-        let data = &msg.unwrap();
-        println!("RECV: {:x?}", &data);
-        let packet = Packet::unpack(&data);
-        return Some(packet);
-    }
-    None
-}
-
-async fn receive_data(notify_char: &Characteristic) -> Option<Vec<u8>> {
-    let mut data: Vec<u8> = Vec::new();
-    let mut updates = notify_char.notify().await.unwrap();
-    while let Some(msg) = updates.next().await {
-        let payload = msg.unwrap();
-        data.extend(&payload);
-        println!("RECV: {:x?}", &payload);
-        if payload.len() == 1 {
-            return Some(data)
-        }
-    }
-    None
-}
-
-async fn support_function_version_info(write_char: &Characteristic, notify_char: &Characteristic) {
+async fn support_function_version_info(camera: &mut Camera) {
     let packet = Packet::with_sid(SID::SUPPORT_FUNCTION_AND_VERSION_INFO);
-    send_packet(write_char, packet).await;
-    let response = receive_packet(notify_char).await.unwrap();
+    camera.send_packet(packet).await.unwrap();
+    let response = camera.receive_packet().await.unwrap();
     let info = SupportFunctionVersionInfo::from_bytes(&response.data);
     dbg!(info);
 }
 
-async fn support_function_info(write_char: &Characteristic, notify_char: &Characteristic, info_type: SupportFunctionInfoType) {
+async fn support_function_info(camera: &mut Camera, info_type: SupportFunctionInfoType) {
     let packet = Packet::with_type(SID::SUPPORT_FUNCTION_INFO, info_type.clone() as u8);
-    send_packet(write_char, packet).await;
-    let response = receive_packet(notify_char).await.unwrap();
+    camera.send_packet(packet).await.unwrap();
+    let response = camera.receive_packet().await.unwrap();
     match &info_type {
         SupportFunctionInfoType::IMAGE_SUPPORT_INFO => {
             let info = ImageSupportInfo::from_bytes(&response.data);
@@ -232,65 +246,65 @@ async fn support_function_info(write_char: &Characteristic, notify_char: &Charac
     }
 }
 
-async fn parameter_read(write_char: &Characteristic, notify_char: &Characteristic, setting: ReadWriteSettingType) {
+async fn parameter_read(camera: &mut Camera, setting: ReadWriteSettingType) {
     let payload = vec![setting as u8, ReadWriteSettingMode::GET_CURRENT_SETTING as u8, 0x00, 0x00, 0x00, 0x00];
     let packet = Packet::with_data(SID::PARAMETER_RW, payload);
-    send_packet(write_char, packet).await;
-    let response = receive_packet(notify_char).await.unwrap();
+    camera.send_packet(packet).await.unwrap();
+    let response = camera.receive_packet().await.unwrap();
     let info = ParameterReadWriteResponse::from_bytes(&response.data);
     dbg!(info);
 }
 
-async fn set_timedate(write_char: &Characteristic, notify_char: &Characteristic) {
+async fn set_timedate(camera: &mut Camera) {
     let now = Utc::now();
     let formatted = now.format("%Y%m%d%H%M%S").to_string();
     let mut bytes = formatted.into_bytes();
     let mut payload: Vec<u8> = vec![2];
     payload.append(&mut bytes);
     let packet = Packet::with_data(SID::TIME_SETTING, payload);
-    send_packet(write_char, packet).await;
-    let response = receive_packet(notify_char).await.unwrap();
+    camera.send_packet(packet).await.unwrap();
+    let response = camera.receive_packet().await.unwrap();
     let info = DateTimeResponse::from_bytes(&response.data);
     dbg!(info);
 }
 
-async fn automatic_photo_download(write_char: &Characteristic, notify_char: &Characteristic) {
+async fn automatic_photo_download(camera: &mut Camera) {
     println!("Auto upload info");
     let packet = Packet::with_sid(SID::IMAGE_AUTO_UPLOAD_INFO);
-    send_packet(write_char, packet).await;
-    let response = receive_packet(notify_char).await.unwrap();
+    camera.send_packet(packet).await.unwrap();
+    let response = camera.receive_packet().await.unwrap();
     if response.data[0] == 0x81 {
         println!("No photo available");
         return;
     }
     println!("Auto upload start");
     let packet = Packet::with_data(SID::IMAGE_AUTO_UPLOAD_START, vec![0;4]);
-    send_packet(write_char, packet).await;
-    let response = receive_packet(notify_char).await.unwrap();
+    camera.send_packet(packet);
+    let response = camera.receive_packet().await.unwrap();
     println!("Auto upload data");
     let num_frames = response.data[3];
     println!("Receiving {} frames", num_frames);
     for frame in 0..50 {
         let frame_num = (frame as u32).to_be_bytes().to_vec();
         let packet = Packet::with_data(SID::IMAGE_AUTO_UPLOAD_DATA, frame_num);
-        send_packet(write_char, packet).await;
-        let data = receive_data(notify_char).await.unwrap();
+        camera.send_packet(packet);
+        let _data = camera.receive_data().await.unwrap();
         println!("Frame: {}", frame);
         thread::sleep(Duration::from_millis(600));
     }
 }
 
-async fn live_view_test(write_char: &Characteristic, notify_char: &Characteristic) {
+async fn live_view_test(camera: &mut Camera) {
     println!("Live view start");
     let packet = Packet::with_type(SID::LIVE_VIEW_START, 0);
-    send_packet(write_char, packet).await;
-    let response = receive_packet(notify_char).await.unwrap();
+    camera.send_packet(packet).await.unwrap();
+    let _response = camera.receive_packet().await.unwrap();
     println!("Live view receive");
     let packet = Packet::with_sid(SID::LIVE_VIEW_RECEIVE);
-    send_packet(write_char, packet).await;
+    camera.send_packet(packet);
     thread::sleep(Duration::from_millis(600));
-    let response = receive_packet(notify_char).await.unwrap();
+    let _response = camera.receive_packet().await.unwrap();
     let packet = Packet::with_sid(SID::LIVE_VIEW_RECEIVE);
-    send_packet(write_char, packet).await;
-    let data = receive_data(notify_char).await.unwrap();
+    camera.send_packet(packet);
+    let _data = camera.receive_data().await.unwrap();
 }
